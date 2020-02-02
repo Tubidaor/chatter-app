dc7abe260e1d2f2cb606e3315bd0b503684c3f36
Juan Baltazar <juan.baltazar1@gmail.com>
2020-02-01 16:55:19 -0800

    added nav bar

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from '../Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="appHeader">
+        <Header/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header/Header.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+
+export default class Header extends Component {
+
+  renderLoginLink() {
+    return (
+      <div className="Header-NotLoggedIn">
+        <Link to='/'>
+          Register
+        </Link>
+        {' - '}
+        <Link to='/login'>
+          Log in
+        </Link>
+      </div>
+    )
+  }
+  render() {
+    return (
+      <nav className='Header'>
+        <h1>
+          <Link to='/'>
+            "Chatter"
+          </Link>
+        </h1>
+        {this.renderLoginLink()}
+      </nav>
+    )
+  }
+}
