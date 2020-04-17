0d3386dac272d954268330ec49d55f8262c2abca
Juan Baltazar <juan.baltazar1@gmail.com>
2020-04-16 22:31:48 -0700

    changed config back to static enendpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
+  API_ENDPOINT: "https://still-hollows-40851.herokuapp.com/api"  || 'http://localhost:8000/api',
   TOKEN_KEY: process.env.TOKEN_KEY,
   user_name: ''
 }
